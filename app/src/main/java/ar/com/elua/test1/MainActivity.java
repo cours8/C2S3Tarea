cfc84eb259c68055670a35068f76f348789340b5
Desencuentro <73045355+Desencuentro@users.noreply.github.com>
2021-07-11 03:49:38 -0300

    Al tocar botón login avisa que no está implementado.

--- app/src/main/java/ar/com/elua/test1/MainActivity.java
+++ app/src/main/java/ar/com/elua/test1/MainActivity.java
@@ -1,13 +1,25 @@
 package ar.com.elua.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AlertDialog.Builder noImplementadoBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btnSaludo);
+        noImplementadoBuilder = new AlertDialog.Builder(this);
+        noImplementadoBuilder.setMessage(R.string.no_implementado_mensaje)
+                .setTitle(R.string.no_implementado_dialogo);
+        btn.setOnClickListener(this::onBtnLoginClick);
+    }
+    public void onBtnLoginClick(View view) {
+        noImplementadoBuilder.create().show();
     }
 }
